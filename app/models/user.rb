35685c59a29b78f15e75bc3661910ec5417c2c0b
Lars Brusletto <larsbrusletto@gmail.com>
2014-11-19 12:58:29 -0800

    refactored code so that the code for the sidebar is placed in layout/_sidebar.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,6 +39,11 @@ class User < ActiveRecord::Base
       email = auth.info.email if email_is_verified
       user = User.where(:email => email).first if email
 
+      #if !user.email_is_verified
+      #  user.skip_confirmation!
+      #  user.save!
+      #end
+
       # Create the user if it's a new registration
       if user.nil?
         user = User.new(
