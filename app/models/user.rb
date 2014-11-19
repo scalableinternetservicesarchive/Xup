137d79624e0625018d2b53449b54b9c6dbf428cc
Lars Brusletto <larsbrusletto@gmail.com>
2014-11-19 14:11:15 -0800

    no change

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,7 @@ class User < ActiveRecord::Base
 
   validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
+
   def self.find_for_oauth(auth, signed_in_resource = nil)
     p 'omni'
     # Get the identity and user if they exist
