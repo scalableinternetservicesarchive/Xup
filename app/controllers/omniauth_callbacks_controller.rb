cf9faec6a577b2ada6afcd130d0010c8cfd23c3c
Yuanyang Zhang <zhangyy1209@gmail.com>
2014-11-24 16:54:18 -0800

    Debuging ec2.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -21,7 +21,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource)
-    p 'halla'
     if resource.email_verified?
       
       stored_location_for(resource) || request.env['omniauth.origin'] || root_path
