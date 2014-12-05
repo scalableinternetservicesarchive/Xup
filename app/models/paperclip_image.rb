d5768cd1d3396f902f81e10b664216f88ea0497d
Lars Brusletto <larsbrusletto@gmail.com>
2014-12-04 20:53:16 -0800

    deleted unnecacary code in paperclip_image.rb in models

--- app/models/paperclip_image.rb
+++ app/models/paperclip_image.rb
@@ -1,10 +1,3 @@
 class PaperclipImage < ActiveRecord::Base
-  # attr_accessible :p_image
-  has_attached_file :p_image, styles: {
-    medium: '300x300>',
-    small: '140x140>',
-    thumb: '64x64!'
-  }
-  validates_attachment_content_type :p_image, :content_type => /\Aimage\/.*\Z/
 
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,14 +4,12 @@ class Profile < ActiveRecord::Base
 									    :medium => '300x300>',
 									    :small => '140x140>',
 									    :thumb => '64x64!'
-  							},
+  							}#,
                 # Here place code for uploading photos on AWS
   							# :storage => :s3,
 	        #   				:s3_credentials => "#{Rails.root}/config/aws.yml",
 	        #   				:path => "Xup/:class/:attachment/:id/:style/:filename",
          #    				:bucket => "cs290"
-	
-
 
   #validates_attachment_content_type :p_image, :content_type => /\Aimage\/.*\Z/
   validates_attachment_content_type :p_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/jfif"]
