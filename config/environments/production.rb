3a8b9884fd537ab23c0549cf84bc81e52e7b5f58
Yuanyang Zhang <zhangyy1209@gmail.com>
2014-12-09 21:20:47 -0800

    little bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # Use memcache gem dalli
-  config.cache_store = :delli_store
+  config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
